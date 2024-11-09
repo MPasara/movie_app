a740c74078e1acdb5e30cd90796d2bb5c6edbe6f
Mladen <mladen.pasara@q.agency>
2024-11-09 17:20:07 +0100

    Moved custom list tile to separate file

---
+++ lib/features/popular/presentation/widgets/popular_movie_list_tile.dart
@@ -0,0 +1,117 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:movie_app/common/domain/router/navigation_extensions.dart';
+import 'package:movie_app/common/presentation/build_context_extensions.dart';
+import 'package:movie_app/common/presentation/image_assets.dart';
+import 'package:movie_app/common/presentation/widgets/genre_chip.dart';
+import 'package:movie_app/common/utils/constants.dart';
+import 'package:movie_app/features/popular/domain/entities/movie.dart';
+import 'package:movie_app/features/popular/presentation/movie_details_page.dart';
+import 'package:movie_app/generated/l10n.dart';
+
+class PopularMovieListTile extends ConsumerWidget {
+  const PopularMovieListTile({
+    super.key,
+   
+    required this.movie,
+  });
+
+ 
+  final Movie movie;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 20),
+      child: InkWell(
+        onTap: () => ref.pushNamed(
+          data: movie,
+          ref.getRouteNameFromCurrentLocation(
+            MovieDetailsPage.routeName,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            children: [
+              SizedBox(
+                width: 100,
+                height: 130,
+                child: CachedNetworkImage(
+                  imageUrl: kImagesBaseUrl + movie.posterImagePath,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              const SizedBox(width: 16),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Flexible(
+                          flex: 5,
+                          child: Text(
+                            movie.title,
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 2,
+                            softWrap: true,
+                            style: TextStyle(
+                              color: context.appColors.defaultColor,
+                              fontSize: 15,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        ),
+                        Flexible(
+                          flex: 1,
+                          child: IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.bookmark_outline,
+                              color: context.appColors.defaultColor,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 12),
+                      child: Row(
+                        children: [
+                          SvgPicture.asset(
+                            ImageAssets.star,
+                          ),
+                          const SizedBox(width: 4),
+                          Text(
+                            S.of(context).movie_rating(
+                                  movie.voteAverage.toStringAsFixed(1),
+                                ),
+                            style: TextStyle(
+                              color: context.appColors.defaultColor,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Wrap(
+                      spacing: 5,
+                      runSpacing: 4,
+                      children: [
+                        for (final genre in movie.genres)
+                          GenreChip(name: genre),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/popular/presentation/widgets/popular_movies_list_widget.dart
+++ lib/features/popular/presentation/widgets/popular_movies_list_widget.dart
@@ -1,16 +1,10 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:flutter/services.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:movie_app/common/domain/router/navigation_extensions.dart';
 import 'package:movie_app/common/presentation/build_context_extensions.dart';
-import 'package:movie_app/common/presentation/image_assets.dart';
-import 'package:movie_app/common/presentation/widgets/genre_chip.dart';
-import 'package:movie_app/common/utils/constants.dart';
 import 'package:movie_app/features/popular/domain/entities/movie.dart';
 import 'package:movie_app/features/popular/domain/notifiers/popular_movies_notifier.dart';
-import 'package:movie_app/features/popular/presentation/movie_details_page.dart';
-import 'package:movie_app/generated/l10n.dart';
+import 'package:movie_app/features/popular/presentation/widgets/popular_movie_list_tile.dart';
 
 class PopularMoviesListWidget extends ConsumerStatefulWidget {
   const PopularMoviesListWidget({
@@ -51,98 +45,7 @@ class _PopularMoviesListWidgetState
             itemCount: widget.movies.length,
             itemBuilder: (context, index) {
               final movie = widget.movies[index];
-              return Padding(
-                padding: const EdgeInsets.only(bottom: 20),
-                child: InkWell(
-                  onTap: () => ref.pushNamed(
-                    data: movie,
-                    ref.getRouteNameFromCurrentLocation(
-                      MovieDetailsPage.routeName,
-                    ),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                    child: Row(
-                      children: [
-                        SizedBox(
-                          width: 100,
-                          height: 130,
-                          child: CachedNetworkImage(
-                            imageUrl: kImagesBaseUrl + movie.posterImagePath,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                        const SizedBox(width: 16),
-                        Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Flexible(
-                                    flex: 5,
-                                    child: Text(
-                                      movie.title,
-                                      overflow: TextOverflow.ellipsis,
-                                      maxLines: 2,
-                                      softWrap: true,
-                                      style: TextStyle(
-                                        color: context.appColors.defaultColor,
-                                        fontSize: 15,
-                                        fontWeight: FontWeight.w600,
-                                      ),
-                                    ),
-                                  ),
-                                  Flexible(
-                                    flex: 1,
-                                    child: IconButton(
-                                      onPressed: () {},
-                                      icon: Icon(
-                                        Icons.bookmark_outline,
-                                        color: context.appColors.defaultColor,
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(bottom: 12),
-                                child: Row(
-                                  children: [
-                                    SvgPicture.asset(
-                                      ImageAssets.star,
-                                    ),
-                                    const SizedBox(width: 4),
-                                    Text(
-                                      S.of(context).movie_rating(
-                                            movie.voteAverage
-                                                .toStringAsFixed(1),
-                                          ),
-                                      style: TextStyle(
-                                        color: context.appColors.defaultColor,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              Wrap(
-                                spacing: 5,
-                                runSpacing: 4,
-                                children: [
-                                  for (final genre in movie.genres)
-                                    GenreChip(name: genre),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              );
+              return PopularMovieListTile(movie: movie);
             },
           ),
         ),
@@ -151,6 +54,7 @@ class _PopularMoviesListWidgetState
   }
 
   Future<void> _handlePullToRefresh() async {
+    HapticFeedback.mediumImpact();
     await ref.read(popularMoviesNotifierProvider.notifier).getPopularMovies(1);
   }
 
